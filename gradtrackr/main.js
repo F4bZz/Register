f83fbe2c5b9819fddd5dc41d8c8428778e41c280
F4bZz <fabrizio.milani@escola.pr.gov.br>
2026-04-20 00:25:59 -0300

    gradtrackr

--- gradtrackr/main.js
+++ gradtrackr/main.js
@@ -2,7 +2,8 @@ let i = 0
 
 while(localStorage.getItem(`work${i}`)){
     let work = JSON.parse(localStorage.getItem(`work${i}`))
-    document.getElementById('work-list').innerHTML += `<div class="work-frame-wrapper"><div class="work-data-container"><h3 class="work-title title" id="title">${work.title}</h3><p class="work-subject subtitle" id="subject">${work.subject}</p><p class="work-date-display subtitle" id="">${'Para '+work.date}</p></div></div>`
+    let date = new Date(work.date)
+    document.getElementById('work-list').innerHTML += `<div class="work-frame-wrapper"><div class="work-data-container"><h3 class="work-title title" id="title">${work.title}</h3><p class="work-subject subtitle" id="subject">${work.subject}</p><p class="work-date-display subtitle" id="">${'Para '+date.toLocaleDateString()}</p></div></div>`
     i++
 }
 
