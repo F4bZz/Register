e2f11612b51a4999bec5718685a1665a33fe513b
F4bZz <fabrizio.milani@escola.pr.gov.br>
2026-04-22 21:18:29 -0300

    gradtrackr

--- gradtrackr/viewPage.js
+++ gradtrackr/viewPage.js
@@ -1,3 +1,5 @@
 const params = new URLSearchParams(window.location.search);
 
+if(!params.get('title')&&!params.get('subject')){window.location.href = './index.html'}
 document.querySelector('.header-title').innerText = params.get('title')
+document.title = `Visualizando ${params.get('title')}`
